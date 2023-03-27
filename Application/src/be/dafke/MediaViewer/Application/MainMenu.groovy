d557a8fd2107d271c5927417304883c313e9c8e9
David C.A. Danneels <daviddafke@msn.com>
2023-03-27 22:52:18 +0200

    implement actionlistener to test Card Switch

--- Application/src/be/dafke/MediaViewer/Application/MainMenu.groovy
+++ Application/src/be/dafke/MediaViewer/Application/MainMenu.groovy
@@ -2,17 +2,33 @@ package be.dafke.MediaViewer.Application
 
 import javax.swing.JMenu
 import javax.swing.JMenuItem
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 
 import static java.util.ResourceBundle.getBundle
 
-class MainMenu extends JMenu {
+class MainMenu extends JMenu implements ActionListener {
     JMenuItem storyView, chapterView
 
     MainMenu() {
         super(getBundle("MediaViewer").getString("MAIN_MENU"))
+
         storyView = new JMenuItem("Stories")
+        storyView.addActionListener this
+
         chapterView = new JMenuItem("Chapter")
+        chapterView.addActionListener this
+
         add storyView
         add chapterView
+    }
+
+    void actionPerformed(ActionEvent e) {
+        Object source = e.getSource()
+        if(source == storyView){
+            Main.switchView(Main.STORIES)
+        } else if(source == chapterView){
+            Main.switchView(Main.CHAPTERS)
+        }
     }
 }

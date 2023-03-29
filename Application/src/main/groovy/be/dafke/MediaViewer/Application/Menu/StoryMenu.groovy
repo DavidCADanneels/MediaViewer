dd9cf4eb9a0111c9502576bafca89ef1592ad265
David C.A. Danneels <daviddafke@msn.com>
2023-03-29 04:29:56 +0200

    disable StoryMenu for now

--- Application/src/main/groovy/be/dafke/MediaViewer/Application/Menu/StoryMenu.groovy
+++ Application/src/main/groovy/be/dafke/MediaViewer/Application/Menu/StoryMenu.groovy
@@ -12,28 +12,23 @@ class StoryMenu extends JMenu {
     StoryMenu() {
         super(getBundle("MediaViewer").getString('STORIES'))
         openStory = new JMenuItem()
-        openStory.addActionListener { e -> readFile() }
+//        openStory.addActionListener { e -> readFile() }
         saveStory = new JMenuItem()
-        saveStory.addActionListener { e -> writeFile() }
+//        saveStory.addActionListener { e -> writeFile() }
     }
 
-    void readFile(){
-        JFileChooser chooser = new JFileChooser()
-        chooser.setMultiSelectionEnabled(false)
-        if(chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-            File file = chooser.getSelectedFile()
-            // TODO: parse xml to Story object and and switch to Chapter View for that Story
-        }
-    }
-
-    void writeFile(){
-        JFileChooser chooser = new JFileChooser()
-        chooser.setMultiSelectionEnabled(false)
-        if(chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-            File file = chooser.getSelectedFile()
-            // TODO: Convert Story object to xml and write to file
-        }
-    }
+//    void readFile(){
+//        JFileChooser chooser = new JFileChooser()
+//        chooser.setMultiSelectionEnabled(false)
+//        if(chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+//            File file = chooser.getSelectedFile()
+//            // TODO: parse xml to Story object and and switch to Chapter View for that Story
+//        }
+//    }
+//
+//    void writeFile(){
+//        StoryOverviewPanel.saveStory()
+//    }
 
 //    void actionPerformed(ActionEvent e) {
 //

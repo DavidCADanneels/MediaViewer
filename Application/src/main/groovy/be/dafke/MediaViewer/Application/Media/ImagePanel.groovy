9db988f1d8f974517b2c5d89801563089b8b7c4b
David C.A. Danneels <daviddafke@msn.com>
2023-04-05 09:43:29 +0200

    Add rescale method

--- Application/src/main/groovy/be/dafke/MediaViewer/Application/Media/ImagePanel.groovy
+++ Application/src/main/groovy/be/dafke/MediaViewer/Application/Media/ImagePanel.groovy
@@ -6,6 +6,8 @@ import be.dafke.MediaViewer.ObjectModel.Stories.Story
 
 import javax.swing.ImageIcon
 import javax.swing.JLabel
+import java.awt.Dimension
+import java.awt.Image
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import javax.swing.JPanel
@@ -13,6 +15,7 @@ import javax.swing.JPanel
 class ImagePanel extends JPanel{
 
     JLabel label
+    boolean fullSize = false
 
     ImagePanel() {
         label = new JLabel()
@@ -31,12 +34,37 @@ class ImagePanel extends JPanel{
             File fileToLoad = new File(startFolder, fileName)
             System.out.println("file: ${fileToLoad.getAbsolutePath()}")
 
-            BufferedImage image = ImageIO.read(fileToLoad)
-            ImageIcon imageIcon = new ImageIcon(image)
-            label.setBounds(0,0,picture.getWidth(),picture.getHeigth())
+
+            BufferedImage bufferedImage = ImageIO.read(fileToLoad)
+
+            ImageIcon imageIcon
+            if(fullSize){
+                imageIcon = new ImageIcon(bufferedImage)
+            } else {
+                Dimension available = getSize()
+                Dimension oldDimension = new Dimension(picture.getWidth(), picture.getHeigth())
+                Dimension newDimension = rescale(oldDimension, available)
+                int availableWidth = available.getWidth()
+                int availableHeight = available.getHeight()
+                System.out.println("Available: ${availableWidth} x ${availableHeight}")
+
+                Image image = bufferedImage.getScaledInstance(newDimension.getWidth().intValue(),newDimension.getHeight().intValue(),Image.SCALE_SMOOTH)
+                imageIcon = new ImageIcon(image)
+            }
             label.setIcon(imageIcon)
         } catch (IOException ex) {
             // handle exception...
         }
+    }
+
+    Dimension rescale(Dimension pictureSize, Dimension available){
+        Double oldWidth = pictureSize.getWidth()
+        Double oldHeigth = pictureSize.getHeight()
+        Double scaleWidth = oldWidth / available.getWidth()
+        Double scaleHeigth = oldHeigth / available.getHeight()
+        Double scale = (scaleWidth > scaleHeigth)?scaleWidth:scaleHeigth
+        Double newWidth = oldWidth / scale
+        Double newHeigth = oldHeigth / scale
+        return new Dimension(newWidth.intValue(),newHeigth.intValue())
     }
 }

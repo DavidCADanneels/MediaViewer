e3bf93f41ad9e39e479a65347624382190bf9465
David C.A. Danneels <daviddafke@msn.com>
2023-04-05 22:42:07 +0200

    Read Creation Date

--- Application/src/main/groovy/be/dafke/MediaViewer/Application/IoTools.groovy
+++ Application/src/main/groovy/be/dafke/MediaViewer/Application/IoTools.groovy
@@ -9,6 +9,10 @@ import com.sun.imageio.plugins.jpeg.JPEGImageReader
 import javax.imageio.ImageIO
 import javax.imageio.ImageReader
 import javax.imageio.stream.ImageInputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.concurrent.TimeUnit
 
 class IoTools {
     static void writeObject(Object object, File file){
@@ -57,9 +61,24 @@ class IoTools {
                     picture.setWidth(width)
                     picture.setHeigth(height)
 
-//                    IIOMetadata metadata = reader.getImageMetadata(0)
-//                    String[] names = metadata.getMetadataFormatNames()
+                    Path path = file.toPath()
+                    BasicFileAttributes attributes = null
+                    try {
+                        attributes = Files.readAttributes(path, BasicFileAttributes.class)
+                    } catch (IOException exception) {
+                        System.out.println("Exception handled when trying to get file " +
+                                "attributes: " + exception.getMessage());
+                    }
+                    long milliseconds = attributes.creationTime().to(TimeUnit.MILLISECONDS);
+                    if((milliseconds > Long.MIN_VALUE) && (milliseconds < Long.MAX_VALUE))
+                    {
+                        Date creationDate =
+                                new Date(attributes.creationTime().to(TimeUnit.MILLISECONDS));
 
+                        System.out.println """\
+File ${path.toString()} created on ${creationDate.getDate()}/${creationDate.getMonth() + 1}/${creationDate.getYear() + 1900} \
+at ${creationDate.getHours()}:${creationDate.getMinutes()}:${creationDate.getSeconds()}"""
+                    }
                     return picture
                 } else {
                     return null

115ed9ce790463fe7b5a1172404cfac6b3bf9bc1
David C.A. Danneels <daviddafke@msn.com>
2023-04-07 23:16:58 +0200

    Add more ShowOptions

--- Application/src/main/groovy/be/dafke/MediaViewer/Application/Media/ImageShowOptionsPanel.groovy
+++ Application/src/main/groovy/be/dafke/MediaViewer/Application/Media/ImageShowOptionsPanel.groovy
@@ -1,22 +1,57 @@
 package be.dafke.MediaViewer.Application.Media
 
-import javax.swing.BoxLayout
+
 import javax.swing.JCheckBox
 import javax.swing.JPanel
 
 class ImageShowOptionsPanel extends JPanel {
 
-    JCheckBox checkBox
+    JCheckBox singleSelectionCheckBox, fullSizeCheckBox, showDetailsCheckBox
     ImagePanel imagePanel
 
     ImageShowOptionsPanel(ImagePanel imagePanel) {
         this.imagePanel = imagePanel
-        checkBox = new JCheckBox("Full size pictures")
-        checkBox.setSelected(false)
-        checkBox.addActionListener { e ->
-            imagePanel.setFullSize(checkBox.isSelected())
+        singleSelectionCheckBox = new JCheckBox("Single Selection")
+        singleSelectionCheckBox.setSelected(true)
+        singleSelectionCheckBox.addActionListener { e ->
+            checkSingleSelection()
         }
-        setLayout new BoxLayout(this, BoxLayout.Y_AXIS)
-        add checkBox
+
+        fullSizeCheckBox = new JCheckBox("Full size pictures")
+        fullSizeCheckBox.setSelected(false)
+        fullSizeCheckBox.addActionListener { e ->
+            fullSizeImage()
+        }
+
+        showDetailsCheckBox = new JCheckBox("Show Media Details")
+        showDetailsCheckBox.setSelected(true)
+        showDetailsCheckBox.addActionListener { e ->
+            checkShowDetails()
+        }
+
+        add singleSelectionCheckBox
+        add fullSizeCheckBox
+        add showDetailsCheckBox
+    }
+
+    void fullSizeImage(){
+        boolean fullSize = fullSizeCheckBox.isSelected()
+        imagePanel.setFullSize(fullSize)
+    }
+
+    void checkShowDetails(){
+        boolean showDetails = showDetailsCheckBox.isSelected()
+        imagePanel.imageDetailPanel.setVisible(showDetails)
+    }
+
+    void checkSingleSelection(){
+        boolean single = singleSelectionCheckBox.isSelected()
+        if(single){
+            fullSizeCheckBox.setEnabled(true)
+        } else {
+            fullSizeCheckBox.setSelected(false)
+            fullSizeImage()
+            fullSizeCheckBox.setEnabled(false)
+        }
     }
 }

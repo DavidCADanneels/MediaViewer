0c1f9d1dbcc6a2836fb775dbb05a8d90b2b92b85
David C.A. Danneels <daviddafke@msn.com>
2023-04-07 11:04:47 +0200

    Add MediaRowSorter

---
+++ Application/src/main/groovy/be/dafke/MediaViewer/Application/Media/MediaRowSorter.groovy
@@ -0,0 +1,20 @@
+package be.dafke.MediaViewer.Application.Media
+
+import javax.swing.table.TableModel
+import javax.swing.table.TableRowSorter
+
+class MediaRowSorter extends TableRowSorter {
+    MediaRowSorter(TableModel tableModel) {
+        super(tableModel)
+    }
+
+//    Comparator<?> getComparator(int column) {
+//        if(isSortable(column)){
+//            super.getComparator(column)
+//        }
+//    }
+
+    boolean isSortable(int column){
+        return column == MediaOverviewDataModel.ID_COL || column == MediaOverviewDataModel.CREATION_DATE_COL
+    }
+}

111c236ab0591d9237904cb257b8934834c13985
David C.A. Danneels <daviddafke@msn.com>
2023-04-07 10:05:15 +0200

    move creationDate from Picture to Media

--- ObjectModel/src/main/groovy/be/dafke/MediaViewer/ObjectModel/Media/Media.groovy
+++ ObjectModel/src/main/groovy/be/dafke/MediaViewer/ObjectModel/Media/Media.groovy
@@ -7,31 +7,32 @@ abstract class Media {
     String subFolderName
     String fileName
     String extension
+    Date creationDate
     // TODO: add Custom @XmlMapper output for 'author' field: use Participant.getId()
 //    Participant author
 
     Media() {
     }
 
-    String getAbsolutePath(File projectRoot){
-        File file = getFile(projectRoot)
-        if(file){
-            return file.name
-        } else null
-    }
+//    String getAbsolutePath(File projectRoot){
+//        File file = getFile(projectRoot)
+//        if(file){
+//            return file.name
+//        } else null
+//    }
 
-    String getFile(File projectRoot){
-        String fullFileName = fileName
-        if(extension != null){
-            fullFileName = "${fullFileName}.${extension}"
-        }
-
-        File folder = projectRoot
-        if(subFolderName != null){
-            folder = new File(projectRoot, subFolderName)
-        }
-        new File(folder, fullFileName)
-    }
+//    String getFile(File projectRoot){
+//        String fullFileName = fileName
+//        if(extension != null){
+//            fullFileName = "${fullFileName}.${extension}"
+//        }
+//
+//        File folder = projectRoot
+//        if(subFolderName != null){
+//            folder = new File(projectRoot, subFolderName)
+//        }
+//        new File(folder, fullFileName)
+//    }
 
     String getSubFolderName() {
         return subFolderName
@@ -47,6 +48,14 @@ abstract class Media {
 
     void setFileName(String fileName) {
         this.fileName = fileName
+    }
+
+    Date getCreationDate() {
+        return creationDate
+    }
+
+    void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate
     }
 
 //    Participant getAuthor() {
--- ObjectModel/src/main/groovy/be/dafke/MediaViewer/ObjectModel/Media/Picture.groovy
+++ ObjectModel/src/main/groovy/be/dafke/MediaViewer/ObjectModel/Media/Picture.groovy
@@ -2,10 +2,23 @@ package be.dafke.MediaViewer.ObjectModel.Media
 
 class Picture extends Media {
     int width, heigth
-    Date creationDate
+    /**
+     * index of Participant in Story.participants List
+     */
+//    Integer owner = null
+    /**
+     * Indices of Participant(s) in Story.participants List
+     */
+//    Integer [] participantsInForeGround, participantsInMiddleGround, participantsBackGround
+//    Boolean localInhabitants = false
+//    Boolean landscape = false
+//    Boolean inside = false
+//    Boolean panorama = false
 
     Picture() {
-
+//        participantsInForeGround = []
+//        participantsInMiddleGround = []
+//        participantsBackGround = []
     }
 
     int getWidth() {
@@ -22,13 +35,5 @@ class Picture extends Media {
 
     void setHeigth(int heigth) {
         this.heigth = heigth
-    }
-
-    Date getCreationDate() {
-        return creationDate
-    }
-
-    void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate
     }
 }
